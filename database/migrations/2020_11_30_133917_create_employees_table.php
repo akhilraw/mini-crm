d39c56e0a0ddfd31d1b47e3f73559b0ef7d400d0
Akhilesh kumar <akhilesh.kumar@vbrigroup.com>
2020-11-30 19:25:58 +0530

    companies and employees table migration with foreign key constraint

---
+++ database/migrations/2020_11_30_133917_create_employees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->unsignedBigInteger('company_id');
+            $table->string('email')->unique();
+            $table->integer('phone')->nullable();
+            $table->timestamps();
+
+            // foreign key constraints
+            $table->foreign('company_id')->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
