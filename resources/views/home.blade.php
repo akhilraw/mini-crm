83de23aee45a731fa1a16f1363f1e74918b20fa5
Akhilesh kumar <akhilesh.kumar@vbrigroup.com>
2020-12-01 13:17:57 +0530

    yajra datatable integration in application

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,23 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="card col-md-6">
+            <div class="card-body">
+                <h5 class="card-title">Companies</h5>
+                <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
+                <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum deleniti voluptates, explicabo molestiae eligendi in eaque necessitatibus cumque ratione. Ipsam!</p>
+                <a href="{{ route('companies.index') }}" class="card-link">Manage Companies</a>
+                <a href="{{ route('companies.create') }}" class="card-link">Add Company</a>
+            </div>
+        </div>
+        <div class="card col-md-6">
+            <div class="card-body">
+                <h5 class="card-title">Employees</h5>
+                <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit optio error eius ducimus soluta, iusto culpa sit, eos officiis dignissimos esse hic enim modi repudiandae.</p>
+                <a href="{{ route('employees.index') }}" class="card-link">Manage Employees</a>
+                <a href="{{ route('employees.create') }}" class="card-link">Add Employee</a>
             </div>
         </div>
     </div>
