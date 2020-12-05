05d342960e12a08045f33b015c3000ab33ff622f
Akhilesh kumar <akhilesh.kumar@vbrigroup.com>
2020-12-05 18:08:19 +0530

    show validation error for column wise and old value in case of error :: in company and employee create form

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -8,29 +8,44 @@
             @csrf
             <div class="form-group">
                 <label for="firstname"> firstname</label>
-                <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Enter firstname" required="required">
+                <input type="text" class="form-control" id="firstname" name="firstname" value="{{ old('firstname') }}" placeholder="Enter firstname" required="required">
+                @if($errors->has('firstname'))
+                <div class="text-danger">{{ $errors->first('firstname') }}</div>
+                @endif
             </div>
             <div class="form-group">
                 <label for="lastname"> lastname</label>
-                <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Enter lastname" required="required">
+                <input type="text" class="form-control" id="lastname" name="lastname" value="{{ old('lastname') }}" placeholder="Enter lastname" required="required">
+                @if($errors->has('lastname'))
+                <div class="text-danger">{{ $errors->first('lastname') }}</div>
+                @endif
             </div>
             <div class="form-group">
                 <label for="email">Email address</label>
-                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email" required="required">
+                <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" aria-describedby="emailHelp" placeholder="Enter email" required="required">
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                @if($errors->has('email'))
+                <div class="text-danger">{{ $errors->first('email') }}</div>
+                @endif
             </div>
             <div class="form-group">
                 <label for="company_id">Company </label>
-                <select class="custom-select" name="company_id">
-                    <option selected>Open this select menu</option>
+                <select class="custom-select" name="company_id" required="required">
+                    <option>Open this select menu</option>
                     @foreach ($companies as $company)
-                    <option value="{{ $company->id }}">{{ $company->name }}</option>
+                    <option value="{{ $company->id }}" @if (old('company_id') == $company->id) selected="selected" @endif>{{ $company->name }}</option>
                     @endforeach
                 </select>
+                @if($errors->has('company_id'))
+                <div class="text-danger">{{ $errors->first('company_id') }}</div>
+                @endif
             </div>
             <div class="form-group">
                 <label for="phone"> phone</label>
-                <input type="number" class="form-control" id="phone" name="phone" placeholder="Enter phone" required="required">
+                <input type="number" class="form-control" id="phone" name="phone" value="{{ old('phone') }}" placeholder="Enter phone" required="required">
+                @if($errors->has('phone'))
+                <div class="text-danger">{{ $errors->first('phone') }}</div>
+                @endif
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
