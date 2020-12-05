d145a627dccaef537b9def435fc24acb17074a61
Akhilesh kumar <akhilesh.kumar@vbrigroup.com>
2020-12-05 18:26:05 +0530

    show validation errors column wise in edit form of both:: employee and company

--- resources/views/company/edit.blade.php
+++ resources/views/company/edit.blade.php
@@ -10,20 +10,32 @@
             <div class="form-group">
                 <label for="name">Company Name</label>
                 <input type="text" class="form-control" id="name" name="name" value="{{ $company->name }}" placeholder="Enter company name" required="required">
+                @if($errors->has('name'))
+                <div class="text-danger">{{ $errors->first('name') }}</div>
+                @endif
             </div>
             <div class="form-group">
                 <label for="email">Email address</label>
                 <input type="email" class="form-control" id="email" name="email" value="{{ $company->email }}" aria-describedby="emailHelp" placeholder="Enter email" required="required">
+                @if($errors->has('email'))
+                <div class="text-danger">{{ $errors->first('email') }}</div>
+                @endif
                 <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
             </div>
             <div class="form-group">
                 <label for="logo">Company Logo</label>
                 <input type="file" class="form-control-file" id="logo" name="logo" value="{{ $company->logo }}" aria-describedby="logoHelp">
                 <small id="logoHelp" class="form-text text-muted">logo dimension should be atleast 100x100.</small>
+                @if($errors->has('logo'))
+                <div class="text-danger">{{ $errors->first('logo') }}</div>
+                @endif
             </div>
             <div class="form-group">
                 <label for="website">Company website</label>
                 <input type="website" class="form-control" id="website" name="website" value="{{ $company->website }}" placeholder="Enter company website" required="required">
+                @if($errors->has('website'))
+                <div class="text-danger">{{ $errors->first('website') }}</div>
+                @endif
             </div>
             <button type="submit" class="btn btn-primary">Update</button>
         </form>
--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -10,15 +10,24 @@
             <div class="form-group">
                 <label for="firstname"> firstname</label>
                 <input type="text" class="form-control" id="firstname" name="firstname" value="{{ $employee->firstname }}" placeholder="Enter firstname" required="required">
+                @if($errors->has('firstname'))
+                <div class="text-danger">{{ $errors->first('firstname') }}</div>
+                @endif
             </div>
             <div class="form-group">
                 <label for="lastname"> lastname</label>
                 <input type="text" class="form-control" id="lastname" name="lastname" value="{{ $employee->lastname }}" placeholder="Enter lastname" required="required">
+                @if($errors->has('lastname'))
+                <div class="text-danger">{{ $errors->first('lastname') }}</div>
+                @endif
             </div>
             <div class="form-group">
                 <label for="email">Email address</label>
                 <input type="email" class="form-control" id="email" name="email" value="{{ $employee->email }}" aria-describedby="emailHelp" placeholder="Enter email" required="required">
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                @if($errors->has('email'))
+                <div class="text-danger">{{ $errors->first('email') }}</div>
+                @endif
             </div>
             <div class="form-group">
                 <label for="company_id">Company </label>
@@ -28,10 +37,16 @@
                     <option value="{{ $company->id }}" @if($company->id == $employee->company_id) selected @endif>{{ $company->name }}</option>
                     @endforeach
                 </select>
+                @if($errors->has('company_id'))
+                <div class="text-danger">{{ $errors->first('company_id') }}</div>
+                @endif
             </div>
             <div class="form-group">
                 <label for="phone"> phone</label>
                 <input type="number" class="form-control" id="phone" name="phone" value="{{ $employee->phone }}" placeholder="Enter phone" required="required">
+                @if($errors->has('phone'))
+                <div class="text-danger">{{ $errors->first('phone') }}</div>
+                @endif
             </div>
             <button type="submit" class="btn btn-primary">Update</button>
         </form>
